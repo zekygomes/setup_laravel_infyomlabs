a50546e9cb0e2416cde490e354bd9bf1a03164eb
Mauricio Pereira <mauricio@csktech.com.br>
2018-06-14 17:53:17 -0300

    add example

--- resources/views/layouts/datatables_js.blade.php
+++ resources/views/layouts/datatables_js.blade.php
@@ -1,6 +1,6 @@
 <!-- Datatables -->
-<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.17/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.17/js/dataTables.bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.colVis.min.js"></script>
